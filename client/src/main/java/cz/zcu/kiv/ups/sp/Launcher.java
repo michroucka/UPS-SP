d1baf30d154ea03ee1b9b7d136f2f1e9302c8de4
Michal Roučka <michroucka7@gmail.com>
2026-01-02 22:53:22 +0100

    fixed

--- client/src/main/java/cz/zcu/kiv/ups/sp/Launcher.java
+++ client/src/main/java/cz/zcu/kiv/ups/sp/Launcher.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        Logger.getInstance().setLogFile("client.log");
+        Logger.info("===== Client application started =====");
         Application.launch(HelloApplication.class, args);
     }
 }
